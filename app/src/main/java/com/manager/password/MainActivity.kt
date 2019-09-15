6399ba030ea44455dd455014fdbbf4481a03678b
Sachin <sachinkmr375@gmail.com>
2019-09-15 14:28:22 +0530

    init repo

---
+++ app/src/main/java/com/manager/password/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.manager.password
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/manager/password/SplashScreenActivity.kt
@@ -0,0 +1,5 @@
+package com.manager.password
+
+class SplashScreenActivity {
+
+}
